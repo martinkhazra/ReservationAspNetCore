ceb0a9261837947b7695234b60cdfc5f5e42183b
martinkhazra <35561032+martinkhazra@users.noreply.github.com>
2020-12-09 11:51:55 +0100

    sum

---
+++ ReservationHallCore/ReservationHallCore/Controllers/MrKhazraController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationHallCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MrKhazraController : ControllerBase
+    {
+        [HttpGet("[action]/{a}/{b}")]
+        public IActionResult Sum(int a, int b) => Ok(a + b);
+    }
+}
